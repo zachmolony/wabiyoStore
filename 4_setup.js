bfe3287655adaf899405f59a0b025af49bed0007
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-07-05 15:43:23 +0100

    added platforms to environment

--- 4_setup.js
+++ 4_setup.js
@@ -15,13 +15,27 @@ function setup() {
     player.scale = PLAYER.scale
     player.restitution = PLAYER.restitution
     player.gravity = PLAYER.gravity
-    
+
     // environment 1
 
     // block A
-    platform1a = createSprite(220, 550, 450, 10)
+    platform1a = createSprite(200, 550, 400, 1)
     platform1a.x = 250
     platform1a.y = 289
     platform1a.shapeColor = color(0, 255, 0)
-    platform1a.setCollider("rectangle", 0, 0, 450, 10)
+    platform1a.setCollider("rectangle", 0, 0, 400, 1)
+
+    // block B
+    platform1b = createSprite(650, 550, 50, 1)
+    platform1b.x = 650
+    platform1b.y = 550
+    platform1b.shapeColor = color(0, 255, 0)
+    platform1b.setCollider("rectangle", 0, 0, 50, 1)
+
+    // block C
+    platform1c = createSprite(800, 550, 50, 1)
+    platform1c.x = 800
+    platform1c.y = 550
+    platform1c.shapeColor = color(0, 255, 0)
+    platform1c.setCollider("rectangle", 0, 0, 50, 1)
 }
