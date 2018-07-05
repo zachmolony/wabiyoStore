c37c7a704f0bed559501599ef00aa490e081cade
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-07-05 12:05:30 +0100

    integrated platform1a

--- 5_draw.js
+++ 5_draw.js
@@ -2,11 +2,17 @@ function draw() {
     clear()
     background(150)
 
+    if (player.position.y > 800) {
+        player.x = 100;
+        player.y = 400;
+        player.velocity.y = 0
+    }
+
     // player controls
     if (keyIsDown(37) === true && player.x != 30) {
         player.x -= 10
     }
-    if (keyWentDown(UP_ARROW) && player.y >= 760) {
+    if (keyWentDown(UP_ARROW) && player.collide(platform1a)) {
         player.velocity.y -= 15;
     }
     if (keyIsDown(39) === true && player.x != width - 40) {
@@ -24,7 +30,7 @@ function draw() {
 }
 
 function mousePressed() {
-    console.log('sprite.position.y = ' + player1.position.y)
+    console.log('sprite.position.y = ' + player.position.y)
     console.log('mouseX = ' + mouseX)
     console.log('mouseY = ' + mouseY)
 }
