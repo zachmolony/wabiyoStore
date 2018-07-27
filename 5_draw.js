58eeadcf9aa538e645179dd17b0aee5f20f63d92
zachmolony <31472522+zachmolony@users.noreply.github.com>
2018-07-27 15:55:43 +0100

    adapted code to accomodate new entities

--- 5_draw.js
+++ 5_draw.js
@@ -2,6 +2,7 @@ function draw() {
     clear()
     background(10)
 
+    // RESPAWNING
     if ((player.position.y > 3000) && (back === false)) {
         player.x = 200;
         player.y = 250;
@@ -16,10 +17,13 @@ function draw() {
         player.y = 400;
     }
 
-    // player controls
+    // PLAYER CONTROLS
+    
+    // left
     if (keyIsDown(37) === true) {
         player.x -= 10
     }
+    // up
     if (keyWentDown(UP_ARROW) && player.collide(platform_a)) {
         player.velocity.y -= 12;
     } else if (keyWentDown(UP_ARROW) && player.collide(platform_b)) {
@@ -36,31 +40,31 @@ function draw() {
         player.velocity.y -= 12;
     }
     
-    // off the right of the screen
-    if (keyIsDown(39) === true && player.x != width + 200) {
+    // right
+    if (keyIsDown(39) === true) {
         player.x += 10
     } else if ((stage1 == true) && (player.position.x > 1500)) {
         // if the user goes past stage 1
         clear()
-        clearFirstStages()
+        clearStage1()
         setup2()
         back = false
     } else if ((stage1 == true) && (player.position.x < 0)) {
         // if the user goes back past stage 1
         clear()
-        clearFirstStages()
-        setup1()
+        clearStage1()
+        setup2()
         back = true
     } else if ((stage2 == true) && (player.position.x > 1500)) {
         // if the user goes past stage 2
         clear()
         back = false
-        clearFirstStages()
+        clearStage2()
         setup3()
     } else if ((stage2 == true) && (player.position.x < 0)) {
         // if the user goes back past stage 2
         clear()
-        clearFirstStages()
+        clearStage2()
         setup1()
         back = true
     } else if ((stage3 == true) && (player.position.x < 0)) {
